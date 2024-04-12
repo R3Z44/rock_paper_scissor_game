084dc2943cf71169cc27c8c5337df2086ee82756
Arash Ariamanesh <reza.moosavi21@gmail.com>
2024-04-12 20:58:26 +0330

    Scores added to the game

--- solution I/src/main.py
+++ solution I/src/main.py
@@ -4,12 +4,16 @@ class RockPaperScissors:
     def __init__ (self, ):
         self.choices = ["rock", "paper", "scissors"]
 
+    player_score = 0
+    computer_score = 0
     def get_player_choice(self):
         player_choice = (input(f"Please enter your choice ({self.choices}) > ")).lower()
         if player_choice in self.choices:
             print(f"Your choice: {player_choice}")
             return player_choice
         print("Please enter a valid choice.")
+        print("------------------------")
+        print("\n")
         return self.get_player_choice()
     
     def get_computer_choice(self):
@@ -17,21 +21,36 @@ class RockPaperScissors:
         print(f"computer_choice: {computer_choice}")
         return computer_choice
     
+        
     def decide_winner(self, player_choice, computer_choice):
         if player_choice == computer_choice:
-            return "It's a Tie!"
+            print("It's a Tie!")
+            print("\n")
+            print(f"Your score: {self.player_score}")
+            print(f"computer score: {self.computer_score}")
+            return True
         
         win_situations = (["rock", "scissors"], ["scissors", "paper"], ["paper", "rock"])
         for situation in win_situations:
             if player_choice == situation[0] and computer_choice == situation[1]:
-                return "congratulations! You won!!!!"
-        return "Sorry, You lost..."
+                self.player_score += 1
+                print("congratulations! You won!!!!")
+                print("\n")
+                print(f"Your score: {self.player_score}")
+                print(f"computer score: {self.computer_score}")
+                return True
+        self.computer_score += 1
+        print("Sorry, You lost...")
+        print("\n")
+        print(f"Your score: {self.player_score}")
+        print(f"computer score: {self.computer_score}")
+        return True
     
     def play(self):
         player_choice = self.get_player_choice()
         computer_choice = self.get_computer_choice()
-        print("\n")
-        print(self.decide_winner(player_choice, computer_choice))
+        self.decide_winner(player_choice, computer_choice)
+        print("------------------------")
         print("\n")
 
 
@@ -42,6 +61,8 @@ while True:
     continue_game = input ("Do you want to play again? Press any key to continue or press 'q' to quit the game. > ")
     if continue_game.lower() == "q" :
         print("\n")
+        print(f"Your score: {self.player_score}")
+        print(f"computer score: {self.computer_score}")
         break
     else:
         print("\n")
